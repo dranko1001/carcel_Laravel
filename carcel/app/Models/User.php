b1b5ba99c74339467e9cf6b474cd10e91e44d3a7
Criss <cristo0595@gmail.com>
2026-03-28 21:45:22 -0500

    dashboard, readme y dependencias

--- carcel/app/Models/User.php
+++ carcel/app/Models/User.php
@@ -11,7 +11,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use App\Models\Visit;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['name', 'email', 'password', 'identification_number', 'role', 'is_active'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
